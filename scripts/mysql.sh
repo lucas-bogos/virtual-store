eff3e58bc2815502c4ce140f3783b59578e4dca1
Lucas Bogos <57922050+lucas-bogos@users.noreply.github.com>
2022-03-29 23:43:34 -0300

    Update mysql.sh

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -1,3 +1,2 @@
-# automatiza a execução de shemas sql
-echo "Qual shema SQL deseja executar? [file.sql]"; read file
+# inicializa o mysql
 mysql -u $DBUSR -p || echo "Digite o usuário do banco de dados: "; read dbuser; mysql -u $dbuser -p
